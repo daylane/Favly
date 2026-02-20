c4e446c0fbf2fcba041ea8e1a34c99fc281b541d
Daylane Silva <111756723+daylaneSilva@users.noreply.github.com>
2026-02-20 17:00:26 -0300

    feat(domain): adicionar entidades Familia e Membro

---
+++ Favly.Domain/Entities/Familia.cs
@@ -0,0 +1,14 @@
+﻿using Favly.Domain.Common.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Favly.Domain.Entities
+{
+    internal class Familia : Entity
+    {
+        public string Nome { get; private set; }
+        public string Convite { get; private set; }
+
+    }
+}
---
+++ Favly.Domain/Entities/Membros.cs
@@ -0,0 +1,18 @@
+﻿using Favly.Domain.Common.Base;
+using Favly.Domain.Common.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Favly.Domain.Entities
+{
+    internal class Membros : Entity
+    {
+        public string Nome { get; private set; }
+        public Papel Permissao { get; private set; }
+        public int FamiliaId { get; private set; }
+        public int UsuarioId { get; private set; }
+
+        public virtual Familia Familia { get; private set; }
+    }
+}
