3e265a985b95a177fd1215a5e94fa94ed0b5c830
Daylane Silva <111756723+daylaneSilva@users.noreply.github.com>
2026-02-20 16:09:30 -0300

    feat: Validações no Guard

--- Favly.Domain/Common/Validations/Guard.cs
+++ Favly.Domain/Common/Validations/Guard.cs
@@ -17,6 +17,16 @@ namespace Favly.Domain.Common.Validations
             if (value is null)
                 throw new DomainException($"{parameterName} não pode ser nulo.");
         }
+        public static void AgainstNullOrWhiteSpace<T>(string value, string parameterName)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new DomainException($"{parameterName} não pode ser nulo ou vazio.");
+        }
+        public static void AgainstInvalidEnum<TEnum>(object value, string parameterName) where TEnum : Enum
+        {
+            if (!Enum.IsDefined(typeof(TEnum), value))
+                throw new DomainException($"O valor '{value}' não é um {typeof(TEnum).Name} válido para o parâmetro {parameterName}.");
+        }
         public static void Against<TException>(bool condition, string message) where TException : Exception
         {
             if (condition)
