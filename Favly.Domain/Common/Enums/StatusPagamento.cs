12acfb00ae2f14036fe51caed9dc9e43ccd2f62c
Daylane Silva <69282025+daylane@users.noreply.github.com>
2026-02-22 20:22:52 -0300

    feat(entity): Criar entity pagamentos

---
+++ Favly.Domain/Common/Enums/StatusPagamento.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Favly.Domain.Common.Enums
+{
+    public enum StatusPagamento { Pendente, Pago, Atrasado }
+}
---
+++ Favly.Domain/Entities/NotificacaoPagamento.cs
@@ -0,0 +1,22 @@
+﻿using Favly.Domain.Common.Base;
+using Favly.Domain.Common.Enums;
+using Favly.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Favly.Domain.Entities
+{
+    public class NotificacaoPagamento : Entity
+    {
+        public Guid FamiliaId { get; private set; }
+        public Guid MembroResponsavelId { get; private set; } 
+        public string Titulo { get; private set; }
+
+        public DinheiroPagamento Valor { get; private set; }
+        public RecorrenciaTarefa Recorrencia { get; private set; }
+
+        public DateTime DataVencimento { get; private set; }
+        public StatusPagamento Status { get; private set; }
+    }
+}
