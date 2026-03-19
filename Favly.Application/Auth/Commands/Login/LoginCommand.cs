6972ec76ea140838a1a1ac922df2c8a4a984e2ab
Daylane Silva <111756723+daylaneSilva@users.noreply.github.com>
2026-03-19 08:28:35 -0300

    fix(auth): Mapea commands

--- Favly.Application/Auth/Commands/Login/LoginCommand.cs
+++ Favly.Application/Auth/Commands/Login/LoginCommand.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Favly.Application.Auth.Commands
+namespace Favly.Application.Auth.Commands.Login
 {
     public record LoginCommand(string Email, string Senha);
 }
